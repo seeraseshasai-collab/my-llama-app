acc6156611210e78c4ca1f5900054c1146585f66
seshasai <seera.seshasai@gmail.com>
2026-01-18 22:34:51 +0530

    Added security password

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
 # App to build a simple chatbot
 import streamlit as st
 from openai import OpenAI
+import os
 
+
+# Password Check
+def check_password():
+    """ Returns true if user had the correct password."""
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if st.session_state["password_input"] == st.secrets["APP_PASSWORD"]:
+            st.session_state["password_correct"] = True
+            del st.session_state["password_input"] # Clean up sensitive data
+        else:
+            st.session_state["password_correct"] = False
+    if st.session_state.get("password_correct",False):
+        return True
+
+    # Show input for password
+    st.text_input("Enter Password", type = "password", on_change=password_entered,key="password_input")
+
+    if "password_correct" in st.session_state:
+        st.error("Password incorrect")
+    return False        
+
+# Run the GATEKEEPER
+if not check_password():
+    st.stop()
 
 st.set_page_config(page_title="My local AI Chatbot", page_icon=":robot_face:")
-st.title("Gemini cloud chatbot")
+st.title("Gemini cloud chatbot-protected chat")
 st.write("This is a simple app to build a chatbot using gemini 2.5 flash lite. Ask a question and get the answer.")
 
 # Get API key from streamlit secrets
